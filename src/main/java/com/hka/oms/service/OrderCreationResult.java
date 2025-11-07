b61e827c473a2cdec4986acecf28670342b6d3f5
alexandru-tatar <alexandru.tatar@yahoo.com>
2025-11-07 22:54:58 +0100

    added wms service

--- src/main/java/com/hka/oms/service/OrderCreationResult.java
+++ src/main/java/com/hka/oms/service/OrderCreationResult.java
@@ -2,5 +2,7 @@ package com.hka.oms.service;
 
 import com.hka.oms.domain.Order;
 import com.hka.oms.payment.dto.PaymentResponse;
+import com.hka.oms.wms.dto.WmsFulfillmentResponse;
 
-public record OrderCreationResult(Order order, String reservationMessage, PaymentResponse payment) {}
+public record OrderCreationResult(Order order, String reservationMessage, PaymentResponse payment,
+                                  WmsFulfillmentResponse fulfillment) {}
